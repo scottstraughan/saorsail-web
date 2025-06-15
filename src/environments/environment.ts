2b941502479bbf0c25da774725628af38346bb20
scottstraughan <42965777+scottstraughan@users.noreply.github.com>
2025-06-15 11:45:29 +0100

    Temp tweaks to popular db location.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   logLevel: LogLevel.info,
   fdroidDatabaseBaseUrl: 'https://f-droid.org/repo',
   fdroidRepositoryUrl: 'https://f-droid.org/repo/index-v2.json',
-  popularDatabaseUrl: 'https://popular-db.saorsail.com/v1/db.json',
+  popularDatabaseUrl: 'https://popular-db.saorsail.com/db.json',
   installServiceEndpoint: 'https://saorsail-main-6e1e5bc.d2.zuplo.dev/install',
 };
