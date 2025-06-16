ba195935d91706200f18216917fb29acc559754d
scottstraughan <42965777+scottstraughan@users.noreply.github.com>
2025-06-16 12:18:48 +0100

    Tweaked debug data name.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@ import { LogLevel } from '../app/shared/services/logger.service';
 export const environment = {
   logLevel: LogLevel.debug,
   fdroidDatabaseBaseUrl: 'https://f-droid.org/repo',
-  fdroidRepositoryUrl: 'http://localhost:4200/assets/test/index-v2.json',
-  popularDatabaseUrl: 'http://localhost:4200/assets/test/popular.json',
+  fdroidRepositoryUrl: 'http://localhost:4200/assets/test-data/index-v2.json',
+  popularDatabaseUrl: 'http://localhost:4200/assets/test-data/popular.json',
   installServiceEndpoint: 'https://saorsail-main-6e1e5bc.d2.zuplo.dev',
 };
